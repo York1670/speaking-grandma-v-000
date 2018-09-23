8182a71d54fce417cb3e13c037d57abefeb351af
Bobbi York <york1670@gmail.com>
2018-09-23 18:46:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(speaking)
-  if
-    
+  if speaking != speaking.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
+  else
+    puts "NO, NOT SINCE 1938!"
   end
 
 end #end of method
