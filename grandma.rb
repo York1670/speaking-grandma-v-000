b3d40b201bba2f0f0b2c20592d8ca123e49793d8
Bobbi York <york1670@gmail.com>
2018-09-23 18:36:27 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,4 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(speaking)
 
-  if speaking != speaking.uppercase
-    puts "HUH?! SPEAK UP, SONNY!"
-  elsif speaking == "I LOVE YOU"
-    puts "I LOVE YOU TO PUMPKIN!"
-  else
-    puts "NO, NOT SINCE 1938!"
-  end#end of if else statement
-
 end #end of method
